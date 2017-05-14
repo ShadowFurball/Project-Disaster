5a7353c2bae623a1be67e6a9fba452110da05158
Tarkan Kemalzade <t.kemalzade@hotmail.co.uk>
2017-05-14 15:03:36 +0100

    Added an attract system
    When the player clicks on the screen the people will stop wondering and
    will start to travel twoards the clicked position. Once they reach their
    target destination the people go back to wandering

---
+++ Assets/Scripts/Attract.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRuby.Tween;
+
+public class Attract : MonoBehaviour {
+
+    const float MOVE_SPEED = 100f;
+    Vector3Tween moveTween;
+
+    public void DoAttract(Vector3 targetPosition)
+    {
+        Vector3 bounds = GetComponent<SpriteRenderer>().bounds.extents;
+
+        while (targetPosition.x < bounds.x || targetPosition.x > Screen.width - bounds.x || targetPosition.y < bounds.y || targetPosition.y > Screen.height - bounds.y) ;
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        float time = distance / MOVE_SPEED;
+
+        moveTween = TweenFactory.Tween(null, transform.position, targetPosition, time, TweenScaleFunctions.Linear, (t) =>
+        {
+            transform.position = t.CurrentValue;
+        }, (t) =>
+        {
+            moveTween = null;
+        });
+
+    }
+
+    public void StopAttract()
+    {
+        if (moveTween != null)
+        {
+            moveTween.Stop(TweenStopBehavior.Complete);
+            moveTween = null;
+        }
+    }
+}
--- Assets/Scripts/ClickAndDrag.cs
+++ Assets/Scripts/ClickAndDrag.cs
@@ -7,12 +7,14 @@ public class ClickAndDrag : MonoBehaviour
     private bool beingDragged = false;
     private GameManager gameManager;
     private Wander wanderScript;
+    private Attract attractScript;
 
 	// Use this for initialization
 	void Start ()
     {
         gameManager = GameManager.instance;
         wanderScript = GetComponent<Wander>();
+        attractScript = GetComponent<Attract>();
 	}
 	
 	// Update is called once per frame
@@ -22,8 +24,15 @@ public class ClickAndDrag : MonoBehaviour
         bool overSprite = GetComponent<SpriteRenderer>().bounds.Contains(mousePosition);
         beingDragged = beingDragged && Input.GetButton("Fire1");
 
+        if (!overSprite && Input.GetButtonDown("Fire1"))
+        {
+            wanderScript.StopWander();
+            attractScript.DoAttract(mousePosition);
+        }
+
         if (overSprite && gameManager.selectedPerson == null)
         {
+            attractScript.StopAttract();
             //If we've pressed down on the mouse (or touched on the iphone)
             if (Input.GetButton("Fire1"))
             {
